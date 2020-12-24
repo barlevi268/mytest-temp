d8d24f60216f069040b8a845d80fa9a7a31bc4ed
Glitch (terem) <none>
2020-12-24 07:11:32 +0000

    😀⛄️ Checkpoint ./public/script.js:12724139/761

--- public/script.js
+++ public/script.js
@@ -4,29 +4,36 @@ var alertModal = {
     successIcon,
     primaryButton,
     secondaryButton,
-    show: alertModal.subView.modal('show'),
+    show: () => alertModal.subView.modal('show'),
+    clear,
+    hide: () => {
+        alertModal.subView.modal('hide')
+        alertModal.clear()
+    },
     display: (message) => {
         if (typeof message == Object) {
             if (message.content) {
-
+                alertModal.content.html(message.content)
             }
             
             if (message.primaryAcion) {
-                alertModal.primaryAcion
-            }
-
-            if (message.primaryAcion) {
-                
+                alertModal.primaryButton.on('click', () => message.primaryAcion)
             }
 
             if (message.secondaryAcion) {
-                
+                alertModal.secondaryButton.on('click', () => message.secondaryAcion)
+            }
+
+            if (message.hideSecondary) {
+                alertModal.secondaryButton.hide()
             }
 
             if (message.icon) {
                 message.icon == 'success' ? alertModal.successIcon.show() : alertModal.successIcon.hide();
-            } else {
-                alertModal.successIcon.hide()
+            }
+
+            if (message.onInit) {
+                message.onInit.call()
             }
 
         } else if (typeof message == String) {
@@ -36,4 +43,7 @@ var alertModal = {
 }
 
 
-$(() => $('#alertModal').modal('show'))
+$(() => {
+    
+    $('#alertModal').modal('show')
+})
