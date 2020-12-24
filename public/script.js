894f224fdaab3e5d6f415a5c96e152ede8d25ca0
Glitch (terem) <none>
2020-12-24 06:59:56 +0000

    🌖🌻 Checkpoint ./public/script.js:12724139/1539

--- public/script.js
+++ public/script.js
@@ -1,8 +1,37 @@
-var alertMoadl = {
-    display = (message) => {
+var alertModal = {
+    subView: $('#alertModal'),
+    content,
+    successIcon,
+    primaryButton,
+    secondaryButton,
+    show: alertModal.subView.modal('show'),
+    display: (message) => {
         if (typeof message == Object) {
+            if (message.content) {
 
-        } else if (typeof message == )
+            }
+            
+            if (message.primaryAcion) {
+                alertModal.primaryAcion
+            }
+
+            if (message.primaryAcion) {
+                
+            }
+
+            if (message.secondaryAcion) {
+                
+            }
+
+            if (message.icon) {
+                message.icon == 'success' ? alertModal.successIcon.show() : alertModal.successIcon.hide();
+            } else {
+                alertModal.successIcon.hide()
+            }
+
+        } else if (typeof message == String) {
+
+        }
     }
 }
 
