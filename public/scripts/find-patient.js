53b4d5770714c389ef5bf3d0e4517a76fc2672d0
Glitch (terem) <none>
2020-12-24 11:46:32 +0000

    📅👾 Checkpoint ./views/findPatient.html:12724139/183 ./public/scripts/find-patient.js:12724139/815 ./public/style.css:12724139/132 ./views/index.html:12724139/145 ./views/menu.html:12724139/57

--- public/scripts/find-patient.js
+++ public/scripts/find-patient.js
@@ -1,4 +1,4 @@
-$(() => {
+function initIdPassportField() {
     var passportField = $('.passport-field')
     var idField = $('.id-field')
 
@@ -7,4 +7,12 @@ $(() => {
         idField.addClass('col-3').removeClass('col-9')
     })
 
+    idField.on('click', () => {
+        idField.addClass('col-9').removeClass('col-3')
+        passportField.addClass('col-3').removeClass('col-9')
+    })
+}
+
+$(() => {
+    initIdPassportField()
 })
