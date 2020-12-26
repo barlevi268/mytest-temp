e1b832ef0f8e646f4ea1ae1560a6e36622feb023
Glitch (terem) <none>
2020-12-26 11:52:07 +0000

    ⛈🙋 Checkpoint ./public/scripts/send-to-moh.js:12724139/1581 ./views/sendToMoh.html:12724139/140

--- public/scripts/send-to-moh.js
+++ public/scripts/send-to-moh.js
@@ -1,3 +1,36 @@
-function makeCards(cards = []) {
-    const Card
+var testCards = {
+    updateList: (cards) => {
+        $.each(cards, (i,val) => {
+
+            var card = testCards.Card.clone()
+
+            card.find('.test-barcode-label').html(val.barcode)
+            card.find('.test-name-label').html(val.patientFullName)
+            card.find('.test-id-label').html(val.patientId)
+            card.find('.test-visit-label').html(val.visit)
+            card.find('.test-tester-label').html(val.tester)
+            card.find('.test-barcode-checkbox').attr('name', `test_checked[${val.id}]`)
+
+            $('.tests-wrapper').append(card)
+
+        })
+    },
+    init: () => {
+        testCards.Card = $('.test-details-card').clone()
+        $('.test-details-card').remove()
+    }
 }
+
+
+const fakeCards = [
+    {
+        id,
+        barcode,
+        patientFullName,
+        patientId,
+        visit
+    }
+]
+$(() => {
+
+})
