663b2eeebe6f890adb43cbf817b02c0baded2b01
Glitch (terem) <none>
2020-12-26 12:04:03 +0000

    🦂🐢 Checkpoint ./public/scripts/send-to-moh.js:12724139/1140 ./views/sendToMoh.html:12724139/210 ./views/newVisit.html:12724139/219 ./views/findPatient.html:12724139/58 ./views/index.html:12724139/58 ./views/menu.html:12724139/58

--- public/scripts/send-to-moh.js
+++ public/scripts/send-to-moh.js
@@ -7,8 +7,8 @@ var testCards = {
             card.find('.test-barcode-label').html(val.barcode)
             card.find('.test-name-label').html(val.patientFullName)
             card.find('.test-id-label').html(val.patientId)
-            card.find('.test-visit-label').html(val.visit)
-            card.find('.test-tester-label').html(val.tester)
+            card.find('.test-visit-label').html(val.visitId)
+            card.find('.test-tester-label').html(val.testerName)
             card.find('.test-barcode-checkbox').attr('name', `test_checked[${val.id}]`)
 
             $('.tests-wrapper').append(card)
@@ -22,15 +22,51 @@ var testCards = {
 }
 
 
-const fakeCards = [
+const fakeTests = [
     {
-        id,
-        barcode,
-        patientFullName,
-        patientId,
-        visit
-    }
+        id:"1",
+        barcode:"8923742",
+        patientFullName:"מנחם כץ",
+        patientId:"056812392",
+        visitId:"8432",
+        testerName:"רוני כהן"
+    },
+    {
+        id:"2",
+        barcode:"8934242",
+        patientFullName:"רותי שוורץ",
+        patientId:"055922392",
+        visitId:"8432",
+        testerName:"רוני כהן"
+    },
+    {
+        id:"3",
+        barcode:"8923744",
+        patientFullName:"דבורה ליפשיץ",
+        patientId:"056812339",
+        visitId:"8432",
+        testerName:"רוני כהן"
+    },
+    {
+        id:"4",
+        barcode:"8923232",
+        patientFullName:"אורי כהן",
+        patientId:"056382392",
+        visitId:"8432",
+        testerName:"רוני כהן"
+    },
+    {
+        id:"5",
+        barcode:"8924042",
+        patientFullName:"שמואל חזן",
+        patientId:"056771392",
+        visitId:"8432",
+        testerName:"רוני כהן"
+    },
 ]
 $(() => {
 
+    testCards.init()
+    
+    testCards.updateList(fakeTests)
 })
