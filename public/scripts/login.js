83ac61a336671cedd6b2d2dbd77449a8a63fb582
Glitch (terem) <none>
2020-12-27 07:47:21 +0000

    🎬💫 Checkpoint ./public/scripts/login.js:12724139/1849

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -4,17 +4,29 @@ function changeLoginView(view) {
     $(`.${view}-form`).removeClass('d-none')
 }
 
-function initPinField() {
-    $('[split-input]').on('input', (e) => {
-        var $target = $(e.target)
-        var inputNumber = $target.attr('split-input')
-        
-        console.log($target.val())
-        console.log($(`[split-input="${inputNumber}"]`))
-        $target.val() != "" ? $(`[split-input="${inputNumber}"]`)[0].focus() : '';
-    })
+var pinField = {
+    init: () => {
+        $('[split-input]').on('input', (e) => {
+            var $target = $(e.target)
+            var inputNumber = parseInt($target.attr('split-input'))
+            
+            if ($target.val() != "") {
+                $target.val(e.originalEvent.data)
+                $(`[split-input="${inputNumber + 1}"]`)[0].focus()
+            } 
+            
+        })
+    },
+    getValue: () => {
+        var merged = []
+        $.each($('[split-input]'), (i,val) => {
+            merged.push($(val).val())
+        })
+
+        return merged.reverse().join('')
+    }
 }
 
 $(() => {
-    initPinField()
+    pinField.init()
 })
