f01cd69e673010614ae26bf656665423d98c7c66
Glitch (terem) <none>
2021-01-04 15:39:49 +0000

    🌔🏩 Checkpoint ./public/test.js:12724139/2736 ./views/test.html:12724139/61

--- public/test.js
+++ public/test.js
@@ -3,8 +3,23 @@ $(() => {
     const codeReader = new ZXing.BrowserMultiFormatReader()
     console.log('ZXing code reader initialized')
 
+    $('[type="file"]').on("change", function(e) {
+        function readURL(input) {
+            if (input.files && input.files[0]) {
+              var reader = new FileReader();
+              reader.onload = e => {
+                $("img").attr("src", e.target.result);
+              };
+      
+              reader.readAsDataURL(input.files[0]);
+            }
+          }
+
+        e.target.files && e.target.files.length ? readURL(e.target) : "";
+    });
+    
     document.getElementById('decodeFile').addEventListener('click', () => {
-        const img = document.getElementById('img')
+        const img = $("img")[0]
         codeReader.decodeFromImage(img).then((result) => {
             console.log(result)
             document.getElementById('result').textContent = result.text
