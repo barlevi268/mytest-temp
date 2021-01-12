6ab187b288c586ee318ec296ad69c12ff3abb49d
Glitch (terem) <none>
2021-01-12 10:24:13 +0000

    🏆👮 Checkpoint ./public/test.js:12724139/2225 ./views/test.html:12724139/664 ./views/newVisit.html:12724139/56

--- public/test.js
+++ public/test.js
@@ -1,37 +1,43 @@
+// $(() => {
+//   const codeReader = new ZXing.BrowserBarcodeReader();
+//   console.log("ZXing code reader initialized");
+
+//   $('[type="file"]').on("change", function(e) {
+//     function readURL(input) {
+//       if (input.files && input.files[0]) {
+//         var reader = new FileReader();
+//         reader.onload = e => {
+//           $("img").attr("src", e.target.result);
+//         };
+
+//         reader.readAsDataURL(input.files[0]);
+//       }
+//     }
+
+//     e.target.files && e.target.files.length ? readURL(e.target) : "";
+//   });
+
+//   document.getElementById("decodeFile").addEventListener("click", e => {
+//     e.preventDefault();
+//     const img = $("img")[0];
+//     codeReader
+//       .decodeFromImageElement(img)
+//       .then(result => {
+//         console.log(result);
+//         document.getElementById("result").textContent = result.text;
+//       })
+//       .catch(err => {
+//         console.error(err);
+//         document.getElementById("result").textContent = err;
+//       });
+//   });
+// });
+
+
 $(() => {
-
-    const codeReader = new ZXing.BrowserBarcodeReader()
-    console.log('ZXing code reader initialized')
-
-    $('[type="file"]').on("change", function(e) {
-        function readURL(input) {
-            if (input.files && input.files[0]) {
-              var reader = new FileReader();
-              reader.onload = e => {
-                $("img").attr("src", e.target.result);
-              };
-      
-              reader.readAsDataURL(input.files[0]);
-            }
-          }
-
-        e.target.files && e.target.files.length ? readURL(e.target) : "";
-    });
-    
-    document.getElementById('decodeFile').addEventListener('click', (e) => {
-        e.preventDefault()
-        const img = $("img")[0]
-        codeReader.decodeFromImageElement(img).then((result) => {
-            console.log(result)
-            document.getElementById('result').textContent = result.text
-        }).catch((err) => {
-            console.error(err)
-            document.getElementById('result').textContent = err
-        })
-        
-    })
-
-
-
+  Quagga.decodeSingle(quaggaDefaultConfig, result => {
+    console.log(result)
+    $('.test-text').html(result)
+  })
 })
 
