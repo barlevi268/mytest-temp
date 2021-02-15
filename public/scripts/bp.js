10cc71d4724c181ec0cc0e0b2c73979ca00a46d8
Glitch (terem) <none>
2021-02-15 21:45:49 +0000

    🌿🌒 Checkpoint ./views/boardingpass.html:12724139/245 ./public/scripts/bp.js:12724139/2282 ./public/style.css:12724139/85

---
+++ public/scripts/bp.js
@@ -0,0 +1,9 @@
+JsBarcode("#barcodePlaceholder", "313521924", {
+  format: "code128",
+  lineColor: "#0aa",
+  width: 3,
+  background:'rgba(0,0,0,0)',
+  lineColor:'#ffffff',
+  height: 60,
+  displayValue: false
+});
