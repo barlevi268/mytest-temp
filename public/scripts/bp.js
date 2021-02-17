9663633e768ef3e4c1f7ad0ca7de4cb3cf9567b6
Glitch (terem) <none>
2021-02-17 08:47:33 +0000

    🐬🔑 Checkpoint ./views/boardingpass.html:12724139/79 ./public/scripts/bp.js:12724139/4075

--- public/scripts/bp.js
+++ public/scripts/bp.js
@@ -1,14 +1,38 @@
-const barcodeSVGConfig = {
-  format: "code128",
-  width: 2,
-  background: "rgba(0,0,0,0)",
-  lineColor: "var(--bg-primary)",
-  height: 120,
-  displayValue: false
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const patient = {
+  id: urlParams.get("id"),
+  firstName: urlParams.get("firstName"),
+  lastName: urlParams.get("lastName"),
+  birthDate: urlParams.get("birthDate")
 };
 
-const fakeID = "313521924";
+$(() => {
+  const barcodeSVGConfig = {
+    format: "code128",
+    width: 2,
+    background: "rgba(0,0,0,0)",
+    lineColor: "var(--bg-primary)",
+    height: 120,
+    displayValue: false
+  };
 
-const svgElmSelector = "#barcodePlaceholder";
+  const fakeID = "313521924";
 
-JsBarcode(svgElmSelector, fakeID, barcodeSVGConfig);
+  const svgElmSelector = "#barcodePlaceholder";
+
+  JsBarcode(svgElmSelector, fakeID, barcodeSVGConfig);
+
+  $("#saveAsImage").on("click", e => {
+    html2canvas($("#bpDiv"), {
+      onrendered: canvas => {
+        theCanvas = canvas;
+
+        canvas.toBlob(blob => {
+          saveAs(blob, "Dashboard.png");
+        });
+      }
+    });
+  });
+  
+});
