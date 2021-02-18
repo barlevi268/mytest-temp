3c1037439f225145da153db68a95d4423bf5a95d
Glitch (terem) <none>
2021-02-18 13:07:09 +0000

    🍘🚈 Checkpoint ./public/scripts/selfservice.js:12724139/3103 ./views/registerSelfService.html:12724139/187

--- public/scripts/selfservice.js
+++ public/scripts/selfservice.js
@@ -1,10 +1,37 @@
+function initCities() {}
+
 async function sendPatiant() {
+  
+  const formData = {
+    clinicCode: "BH",
+    IdNum: "336878467",
+    FirstName: "שרה",
+    LastName: "גרינוולד",
+    DOB: "12/12/2012",
+    Gender: "נ",
+    HMO: 3,
+    cityCode: 383,
+    streetName: "הפסגה",
+    streetNumber: 64,
+    telHome: "026411157",
+    mobile: "0504166626",
+    email: "",
+    Comment: "testg2"
+  };
+  
+  const networkCheck = await fetch("https://patients.terem.com/myvisit/covidLab/savePatient");
+  
+  const actualRequest = await fetch("https://patients.terem.com/myvisit/covidLab/savePatient", {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      username:'adminLab19',
+      pw:'labws12!'
+    }
+  });
+  
+    
   
 }
 
-$(() => {
-
-  
-  
-
-});
+$(() => {});
