0ca98371c5da17454f641b8fe85cfe11fb7549e9
Glitch (terem) <none>
2021-07-10 13:16:22 +0000

    👶🚇 Checkpoint ./public/scripts/new-visit.js:12724139/13165

--- public/scripts/new-visit.js
+++ public/scripts/new-visit.js
@@ -1,20 +1,20 @@
 function initBarcodeDetect() {
-  $('.barcode-loader').hide()
-  
+  $(".barcode-loader").hide();
+
   function detectCodeInImage(src) {
     const codeField = $('[name="test_barcode"]');
     var code = "";
 
     var config = quaggaDefaultConfig;
 
-    $('.barcode-loader').show('fast');
-    
+    $(".barcode-loader").show("fast");
+
     decodeBarcode(src, config, result => {
-      result ? code = result : "";
-      console.log(result)
-      
-      $('.barcode-loader').hide('fast');
-      
+      result ? (code = result) : "";
+      console.log(result);
+
+      $(".barcode-loader").hide("fast");
+
       codeField.attr("placeholder", "הזן ברקוד");
       codeField.val(code);
     });
@@ -27,68 +27,83 @@ function initBarcodeDetect() {
   });
 }
 
-function initWebcamWidget() {
-        var width = 320;
-        var height = 0;
+var webcam = (function() {
+  var width = 320;
+  var height = 0;
 
-        var streaming = false;
+  var streaming = false;
 
-        var video = null;
-        var canvas = null;
-        var photo = null;
-        var startbutton = null;
+  var video = document.getElementById("video");
+  var canvas = document.getElementById("canvas");
+  var photo = document.getElementById("photo");
+  var startbutton = document.getElementById("startbutton");
 
-        var startup = function() {
-            video = document.getElementById('video');
-            canvas = document.getElementById('canvas');
-            photo = document.getElementById('photo');
-            startbutton = document.getElementById('startbutton');
-
-            navigator.mediaDevices.getUserMedia({
-                    video: true,
-                    audio: false
-                })
-                .then(function(stream) {
-                    video.srcObject = stream;
-                    video.play();
-                })
-                .catch(function(err) {
-                    console.log("An error occurred: " + err);
-                });
-
-            video.addEventListener('canplay', function(ev) {
-                if (!streaming) {
-                    height = video.videoHeight / (video.videoWidth / width);
-
-                    if (isNaN(height)) {
-                        height = width / (4 / 3);
-                    }
-
-                    video.setAttribute('width', width);
-                    video.setAttribute('height', height);
-                    canvas.setAttribute('width', width);
-                    canvas.setAttribute('height', height);
-                    streaming = true;
-                }
-            }, false);
-
-        }()
-}
-
-function initBarcodeImagePicker() {
-  if (!isMobile) {
-    var field = $('[for="uploadBarcode"]')
-    field.on('click', e => {
-      e.preventDefault()
-      alertModal.display({
-        modalId:'webcamModal',
-        primaryLabel:"צלם",
-        primaryAction: () => handleCapture(),
-        afterInit: () => initWebcamWidget()
+  function startup() {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true,
+        audio: false
       })
-    })
+      .then(function(stream) {
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch(function(err) {
+        console.log("An error occurred: " + err);
+      });
+
+    video.addEventListener(
+      "canplay",
+      function(ev) {
+        if (!streaming) {
+          height = video.videoHeight / (video.videoWidth / width);
+
+          if (isNaN(height)) {
+            height = width / (4 / 3);
+          }
+
+          video.setAttribute("width", width);
+          video.setAttribute("height", height);
+          canvas.setAttribute("width", width);
+          canvas.setAttribute("height", height);
+          streaming = true;
+        }
+      },
+      false
+    );
   }
-}
+  
+  function handleCapture() {
+    var context = canvas.getContext('2d');
+    canvas.width = width;
+    canvas.height = height;
+    context.drawImage(video, 0, 0, width, height);
+
+    var data = canvas.toDataURL('image/png');
+    photo.setAttribute('src', data);
+  }
+
+  function initBarcodeImagePicker() {
+    if (!isMobile) {
+      var field = $('[for="uploadBarcode"]');
+      field.on("click", e => {
+        e.preventDefault();
+        alertModal.display({
+          modalId: "webcamModal",
+          primaryLabel: "צלם",
+          primaryAction: () => handleCapture(),
+          afterInit: () => startup()
+        });
+      });
+    }
+  }
+  
+  return {
+    init: function() {
+      initBarcodeImagePicker()
+    }
+  };
+})();
 
 const fakeVisit = {
   fullName: "חיים רפאלי",
@@ -118,9 +133,9 @@ function initConditionalFields() {
     switch ($target.attr("name")) {
       case "testType":
         if ($('[name="testType"]:checked').val() == "PCR") {
-          $(".pcr-test-types").show('fast');
+          $(".pcr-test-types").show("fast");
         } else {
-          $(".pcr-test-types").hide('fast');
+          $(".pcr-test-types").hide("fast");
         }
         break;
       case "pcrTestType":
