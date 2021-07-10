9d82e5670ae90c55e18529cb2a7b8609360c8c80
Glitch (terem) <none>
2021-07-10 12:54:19 +0000

    🏔🐵 Checkpoint ./public/scripts/new-visit.js:12724139/2188 ./views/newVisit.html:12724139/53277

--- public/scripts/new-visit.js
+++ public/scripts/new-visit.js
@@ -27,13 +27,64 @@ function initBarcodeDetect() {
   });
 }
 
+function initWebcamWidget() {
+        var width = 320;
+        var height = 0;
+
+        var streaming = false;
+
+        var video = null;
+        var canvas = null;
+        var photo = null;
+        var startbutton = null;
+
+        var startup = function() {
+            video = document.getElementById('video');
+            canvas = document.getElementById('canvas');
+            photo = document.getElementById('photo');
+            startbutton = document.getElementById('startbutton');
+
+            navigator.mediaDevices.getUserMedia({
+                    video: true,
+                    audio: false
+                })
+                .then(function(stream) {
+                    video.srcObject = stream;
+                    video.play();
+                })
+                .catch(function(err) {
+                    console.log("An error occurred: " + err);
+                });
+
+            video.addEventListener('canplay', function(ev) {
+                if (!streaming) {
+                    height = video.videoHeight / (video.videoWidth / width);
+
+                    if (isNaN(height)) {
+                        height = width / (4 / 3);
+                    }
+
+                    video.setAttribute('width', width);
+                    video.setAttribute('height', height);
+                    canvas.setAttribute('width', width);
+                    canvas.setAttribute('height', height);
+                    streaming = true;
+                }
+            }, false);
+
+        }()
+}
+
 function initBarcodeImagePicker() {
   if (!isMobile) {
     var field = $('[for="uploadBarcode"]')
     field.on('click', e => {
       e.preventDefault()
       alertModal.display({
-        modalId:'webcamModal'
+        modalId:'webcamModal',
+        afterInit: () => {
+          initWebcamWidget()
+        }
       })
     })
   }
