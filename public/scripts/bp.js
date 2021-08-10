d484a139d31eb1d8c47b744aa1e3318d70d39ee7
Glitch (terem) <none>
2021-08-10 08:57:30 +0000

    ⏰🐙 Checkpoint ./public/scripts/bp.js:12724139/1896 ./views/bpcard.html:12724139/682

--- public/scripts/bp.js
+++ public/scripts/bp.js
@@ -8,6 +8,26 @@ const patient = {
   birthDate: urlParams.get("birthDate")
 };
 
+
+// print elemnt function //
+function printElm(elem){
+    var mywindow = window.open('', 'PRINT', 'height=400,width=600');
+
+    mywindow.document.write('<html><head><title>' + document.title  + '</title>');
+    mywindow.document.write('</head><body >');
+    mywindow.document.write('<h1>' + document.title  + '</h1>');
+    mywindow.document.write(document.getElementById(elem).innerHTML);
+    mywindow.document.write('</body></html>');
+
+    mywindow.document.close(); // necessary for IE >= 10
+    mywindow.focus(); // necessary for IE >= 10*/
+
+    mywindow.print();
+    mywindow.close();
+
+    return true;
+}
+
 // make png function //
 function makePNGfromSVG(svgCode) {
   const svg = svgCode;
@@ -127,5 +147,7 @@ $(() => {
 
     
   });
+  
+  $('#printBp').on('click', e => printElm(document.getElementById('bpDiv')))
 
 });
