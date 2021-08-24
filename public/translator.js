78f6e6bf24a17996ef0c8568793532f996a1a602
Glitch (terem) <none>
2021-08-24 10:00:45 +0000

    🐗🦄 Checkpoint ./public/translator.js:12724139/407

--- public/translator.js
+++ public/translator.js
@@ -1,3 +1,10 @@
-var initLocalizations = function() {
-  var labels = document.querySelectorAll('')
+var initLocalizations = async function() {
+  var labels = document.querySelectorAll('trns')
+  var inputs = document.querySelectorAll('input')
+  var lang = localStorage.getItem('lang') ? localStorage.getItem('lang') : 'HE'
+  var langRequest = await fetch(`/translations/${lang}`)
+  var langResults = await langRequest.json()
+  labels.forEach(label => {
+    var value = label.textContent
+  })
 }
