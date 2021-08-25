11c717d0ec08d5d3fb7dd7632f920477544c6d30
Glitch (terem) <none>
2021-08-25 09:35:55 +0000

    🖇🚣 Checkpoint ./public/script.js:12724139/19306 ./views/newVisit.html:12724139/32393

--- public/script.js
+++ public/script.js
@@ -4,24 +4,24 @@ var localizations = (async function() {
   var elements = document.querySelectorAll("[trns]");
   var inputs = document.querySelectorAll("input");
   var selects = document.querySelectorAll("select");
-  
+
   var lang = localStorage.getItem("lang") ? localStorage.getItem("lang") : "HE";
-  
+
   var langRequest = await fetch(`/translations/${lang}.json`);
   var langResults = await langRequest.json();
-  
+
   document.querySelector("html").setAttribute("dir", langResults.dir);
-  
+
   const translateValue = value => {
     var text = langResults.values[value];
     return text ? text : value;
   };
-  
+
   elements.forEach(item => {
     item.textContent = translateValue(item.textContent);
   });
 
-  [...selects,...inputs].forEach(input => {
+  [...selects, ...inputs].forEach(input => {
     input.setAttribute(
       "placeholder",
       translateValue(input.getAttribute("placeholder"))
@@ -30,12 +30,15 @@ var localizations = (async function() {
 
   function _init() {
     var selectLang = document.querySelector(".floating-lang");
-    selectLang.value = lang;
-    selectLang.addEventListener("change", e => {
-      localStorage.setItem("lang", e.target.value);
-      location.reload();
-    });
-    window['_translations'] = langResults
+    if (selectLang) {
+      selectLang.value = lang;
+      selectLang.addEventListener("change", e => {
+        localStorage.setItem("lang", e.target.value);
+        location.reload();
+      });
+    }
+
+    window["_translations"] = langResults;
     initSelect2();
   }
 
@@ -263,7 +266,7 @@ function initSelect2() {
     var displayMode = $(val).attr("select2-type");
     var settings = {
       width: "100%",
-      dir:_translations.dir,
+      dir: _translations.dir,
       placeholder: $(val).attr("placeholder")
     };
     displayMode != "search"
