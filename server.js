319c2411ed06626bbce98e09569588e89a2e391e
Glitch (terem) <none>
2022-01-06 06:01:11 +0000

    ⛸🌸 Checkpoint ./public/script.js:12724139/8489 ./public/utils.js:12724139/7521 ./views/index.ejs:12724139/2514 ./views/componenets/alert-modal.ejs:12724139/1405 ./views/componenets/header.ejs:12724139/1525 ./server.js:12724139/710

--- server.js
+++ server.js
@@ -17,34 +17,7 @@ app.get("/menu", (request, response) => {
   response.sendFile(__dirname + "/views/menu.html");
 });
 
-app.get("/findPatient", (request, response) => {
-  response.sendFile(__dirname + "/views/findPatient.html");
-});
-
 app.get("/newVisit", (request, response) => {
   response.sendFile(__dirname + "/views/newVisit.html");
 });
 
-app.get("/moh", (request, response) => {
-  response.sendFile(__dirname + "/views/sendToMoh.html");
-});
-
-app.get("/selfService", (request, response) => {
-  response.sendFile(__dirname + "/views/registerSelfService.html");
-});
-
-app.get("/bp", (request, response) => {
-  response.sendFile(__dirname + "/views/bpcard.html");
-});
-
-app.get("/desk", (request, response) => {
-  response.sendFile(__dirname + "/views/desk.html");
-});
-
-app.post("/test", (request, response) => {
-  console.log(request)
-});
-
-app.get("/test", (request, response) => {
-  response.sendFile(__dirname + "/views/test.html");
-});
