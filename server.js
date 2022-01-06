1528cdb91f6c3b3e8b2c596ded05b3d95b150ebd
Bar Levi <barlevi2@gmail.com>
2022-01-06 08:40:59 +0200

    chages

--- server.js
+++ server.js
@@ -20,3 +20,7 @@ app.get("/profile", (req, res) => {
 app.get("/newVisit", (req, res) => {
   res.render('pages/new-visit');
 });
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+  });
