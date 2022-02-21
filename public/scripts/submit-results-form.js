5cdf46afc286ab712408a6b320eebb9e03943f9e
Shtanko Sasha <htanko52@gmail.com>
2022-02-21 16:12:54 +0200

    * Sending result doesnt work #2320943670

--- public/scripts/submit-results-form.js
+++ public/scripts/submit-results-form.js
@@ -1,9 +1,19 @@
 var testID;
 var btnSubmissionForm = $(".btn-submission-form");
 var uploadResultField = $('.upload-result');
-var photoResultBtn = $('[name=photo_result]');
+var photoResultBtn = $('[name="photo_result"]');
 var photoResultFile;
 
+/**
+ * It returns the value of the radio button that is selected.
+ */
+const selectedTestResult = () => $('[name="result"]:checked').val();
+
+/**
+ * It takes the file that the user has uploaded and converts it to a base64 string.
+ * @param event - The event object that was triggered.
+ * @returns The image file that was uploaded.
+ */
 async function setPhotoResult(event) {
   if (event && event.target.files) {
     photoResultFile = event.target.files[0];
@@ -19,29 +29,104 @@ async function setPhotoResult(event) {
   uploadResultField.addClass('d-none')
 }
 
+/**
+ * The function is called when the page is loaded.
+ * It checks if the teskitIdParam is set.
+ * If it is, it sets the testID to the value of teskitIdParam.
+ * Otherwise, it redirects the page to the root of the site
+ * @returns The return value is the result of the function call.
+ */
 function initTestID() {
-  const pathname = location.pathname;
-  const testIdFromPathName = pathname.replace(/^\//).replace(/\/test/);
-  if (testIdFromPathName) {
-    testID = testIdFromPathName;
+  if (teskitIdParam) {
+    testID = teskitIdParam;
     return;
   }
   location.href = '/';
 }
 
-function testSubmissionClickHandler() {
+function handleFormInvalid() {
+  alertModal.display({
+    modalId: 'alertModal',
+    icon: "error",
+    content: "נא להעלות את תמונת הבדיקה ולבחור את התוצאה",
+    hideSecondary: true,
+    primaryLabel: "הבנתי",
+  });
+}
+
+function handleFormSubmit() {
+
+  /**
+   * Given the selectedTestResult, return the corresponding test result
+   * @returns The result of the test.
+   */
+  const getSetectedTestResult = () => {
+    if (selectedTestResult() === 'c') {
+      return 'negative'
+    } else if (selectedTestResult() === 'ct') {
+      return 'positive'
+    } else {
+      return 'defected'
+    }
+  }
+
+  let formData = new FormData();
+
+  formData.append("image", photoResultBtn[0].files[0]);
+  formData.append('display_type', selectedTestResult());
+  formData.append('status', getSetectedTestResult());
+
+  var requestOpbject = new RequestObject('POST', formData, false, 'text', false);
+
+  $('.result-input').addClass('d-none')
+  btnSubmissionForm.prop('disabled', true).text('שולח תוצאה...');
+
+  request(`/api/test-kit/${teskitIdParam}/upload-result`, requestOpbject)
+    .then(res => handleSubmitSuccess())
+    .catch((res, ajax, status) => handleSubmitFailure())
+
+}
+
+function handleSubmitSuccess() {
   alertModal.display({
     modalId: 'alertModal',
     icon: "success",
-    content: "תוצאה נשלחה בהצלחה",
+    content: "ביצוע בדיקה נשלח",
     hideSecondary: true,
     primaryLabel: "סיום",
     primaryAction: () => (location.href = '/profile'),
   });
 }
 
+function handleSubmitFailure() {
+  $('.result-input').removeClass('d-none')
+  btnSubmissionForm.prop('disabled', false).text('סיים ושלח');
+
+  alertModal.display({
+    modalId: 'alertModal',
+    content: "לא ניתן להעלות את תוצאת הבדיקה, אנה נסה שוב",
+    hideSecondary: true,
+    primaryLabel: "הבנתי",
+  });
+}
+
+/**
+ * If the user has selected a photo and a test result, return true
+ * @returns The form is being returned.
+ */
+const isValidFormResults = () => {
+  return photoResultBtn[0].files?.length !== 0 && selectedTestResult() !== undefined;
+}
+
+function testSubmissionClickHandler() {
+
+  isValidFormResults() ? handleFormSubmit() : handleFormInvalid();
+
+}
+
+
 $(() => {
   initTestID();
-  photoResultBtn.on("change", async event => setPhotoResult(event));
+  photoResultBtn.on("change", async e => setPhotoResult(e));
   btnSubmissionForm.on("click", async e => testSubmissionClickHandler(e));
 });
